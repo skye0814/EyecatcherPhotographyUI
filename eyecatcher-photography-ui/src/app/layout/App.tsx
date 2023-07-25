527d69918b7414bc73dc240b660aee6858b13522
John Matthew Arquelola <140248020+spicyskye@users.noreply.github.com>
2023-07-25 08:17:29 +0000

    Route navigation

--- eyecatcher-photography-ui/src/app/layout/App.tsx
+++ eyecatcher-photography-ui/src/app/layout/App.tsx
@@ -2,28 +2,30 @@ import React from 'react';
 import NavBar from './NavBar';
 
 // import the library
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from '@fortawesome/fontawesome-svg-core';
 
 // import your icons
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import {
+  BrowserRouter,
+  Routes, //replaces "Switch" used till v5
+  Route,
+  Router,
+} from "react-router-dom";
+import Services from './Services';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='container'>
+            <Routes>
+              <Route path="/" element='' />
+              <Route path="/services" element={<Services />} />
+            </Routes>
+        </div>
       
     </div>
   );
