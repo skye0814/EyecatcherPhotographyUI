d8396446e9c9825283ba861acadc263fc5638448
John Matthew Cruz Arquelola <arquelolajm@gmail.com>
2023-09-10 18:12:04 +0800

    Next page function

--- eyecatcher-photography-ui/src/app/layout/Products.tsx
+++ eyecatcher-photography-ui/src/app/layout/Products.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Breadcrumb, Button } from "semantic-ui-react";
 import { useSearchParams } from "react-router-dom";
 import { PagedRequest } from "../models/PagedRequest";
@@ -14,50 +14,68 @@ const sections = [
   ];
 
 export default function Products(){
+    const [showNextPage, setShowNextPage] = useState(false);
+    const [showPrevPage, setShowPrevPage] = useState(false);
     const [products, setProducts] = useState<PagedResponse<Product[]>>();
+    const [error, setError] = useState(null);
+    const [searchParams]: any = useSearchParams();
+    const [queryParams, setQueryParams] = useState<PagedRequest>({
+        pageNumber: 1,
+        pageSize: 2,
+        sortBy: 'productName',
+        productCategoryId: !isNaN(parseInt(searchParams.get('productCategoryId'))) ? searchParams.get('productCategoryId') : null
+    });
 
     const fetchProduct = (queryParams: PagedRequest) => {
         get(`/Product/Products?pageNumber=${queryParams.pageNumber}&pageSize=${queryParams.pageSize}
             &sortBy=${queryParams.sortBy}&productCategoryId=${queryParams.productCategoryId}`)
         .then((response)=> {
             setProducts(response);
-            console.log(products);
         })
         .catch((err)=>{
             setError(err);
-            console.log(error);
         })
     }
-    const [error, setError] = useState(null);
-    const [searchParams]: any = useSearchParams();
-    const [queryParams, setQueryParams] = useState<PagedRequest>({
-        pageNumber: 1, //!isNaN(parseInt(searchParams.get('pageNumber'))) ? searchParams.get('pageNumber') : 1,
-        pageSize: 2, //!isNaN(parseInt(searchParams.get('pageSize'))) ? searchParams.get('pageSize') : 10,
-        sortBy: 'productName', //searchParams.get('sortBy'),
-        productCategoryId: !isNaN(parseInt(searchParams.get('productCategoryId'))) ? searchParams.get('productCategoryId') : null
-    });
 
     const nextPage = (page: number) => {
         let updatedQueryParams: PagedRequest = {...queryParams};
-
         updatedQueryParams.pageNumber = page;
-        setQueryParams(updatedQueryParams);
-        fetchProduct(updatedQueryParams);
 
-        // not working at 2, works at 3
-        if (products?.pageNumber == 2){
-            console.log("test");
+        if(products){
+            if(updatedQueryParams.pageNumber > products.totalPages){
+                updatedQueryParams.pageNumber = page - 1;
+                setQueryParams(updatedQueryParams);
+            }
+            else{
+                setQueryParams(updatedQueryParams);
+                fetchProduct(updatedQueryParams);
+            }
         }
     }
     
     useEffect(() => {
+        // Fetch products on render
         fetchProduct(queryParams);
+
+        // Disable next page button
+        if(products){
+            if(queryParams.pageNumber === products.totalPages){
+                setShowNextPage(false);
+            }
+        }
+
     }, []);
 
     return (
         <div className="container">
             <Breadcrumb icon='right angle' sections={sections} style={{margin: '10px 0 18px 0'}} />
-            <button onClick={()=>{nextPage(queryParams.pageNumber + 1)}}>Page up</button>
+            <button 
+                id="next-page-btn" 
+                onClick={()=>{nextPage(queryParams.pageNumber + 1)}} 
+                disabled={showNextPage}
+            >
+                Page up
+            </button>
 
             <p>{queryParams.pageNumber}</p>
             <div>{products?.pageNumber}</div>
