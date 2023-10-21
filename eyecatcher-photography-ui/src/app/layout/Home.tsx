6291c762130f7f1d32e5c491503103283c2f1a3d
John Matthew Cruz Arquelola <arquelolajm@gmail.com>
2023-10-21 16:02:29 +0800

    Change navbar and home page styling

--- eyecatcher-photography-ui/src/app/layout/Home.tsx
+++ eyecatcher-photography-ui/src/app/layout/Home.tsx
@@ -1,86 +1,136 @@
-import '../styles/home.css'
+import React from 'react';
+import { Button, Divider } from '@mui/joy';
+import BioCard from '../common/BioCard';
+// import '../styles/home.css'
+import '../styles/homepage.css'
 import { Typography } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Home(){
     return(
+        
         <div style={{marginTop: '80px'}}>
-            <div className="grid-container">
-                <div className="grid-item item1">
-                    <div className='intro-quote1'>
+            <div className='grid-container'>
+                <div className='grid-item item1'>
+                    <div>
                         <Typography className='intro-quote'>
-                        Photography is the <br/> <strong className='intro-quote'> art of froz</strong>
-                        <span 
-                            className='intro-quote' 
-                            style={{
-                                textDecoration: 'underline',
-                                textUnderlineOffset: '10px',
-                                textDecorationThickness: '4px'
-                            }}
-                        >
-                            <strong className='intro-quote'>en time</strong>
-                        </span>
-                        </Typography>
-                    </div>
-                </div>
-                <div className="grid-item item2">
-                    <img src='/images/intro-image1.jpg' alt='Jasper and Natasha' className='intro-image1'/>
-                </div>
-                <div className="grid-item item3">
-                    <img src='/images/intro-image2.jpg' alt='Rodney and Zyrah' className='intro-image2'/>
-                </div>
-                <div className="grid-item item4">
-                    <div className='intro-quote2'>
-                        <Typography className='intro-quote'>
-                        where the science of light  <br/>
-                        <span 
-                            className='intro-quote' 
-                            style={{
-                                textDecoration: 'underline',
-                                textUnderlineOffset: '10px',
-                                textDecorationThickness: '4px'
-                            }}
-                        >
-                            meets t
-                        </span>
-                        he <strong className='intro-quote'>art of emotion</strong>
-                        </Typography>
-                    </div>
-                    <button className='intro-button'>Learn more</button>
-                </div>
-            </div>
-            <div className='container'>
-                <div className='founder-message-box'>
-                    <img className='founder-image founder-message-box-element' src='/images/founder-image.jpg' />
-                    <div className='founder-message-div founder-message-box-element'>
-                        <span className='founder-message-title'>
-                            <Typography className='intro-quote'>
-                            I am Jes Sta Isabel-Pacios  <br/>
+                            Photography is the <br/> <strong className='intro-quote colored-font'> art of froz</strong>
                             <span 
                                 className='intro-quote' 
                                 style={{
                                     textDecoration: 'underline',
                                     textUnderlineOffset: '10px',
-                                    textDecorationThickness: '4px'
+                                    textDecorationThickness: '4px',
+                                    color: 'var(--blue)'
                                 }}
                             >
-                                found
+                                <strong className='intro-quote colored-font'>en time</strong>
                             </span>
-                            er of Eyecatcher Photography
-                            </Typography>
-                        </span>
-                        <span className='founder-message'>
-                            I am Jes Sta Isabel-Pacios, the founder of Eyecatcher Photography. 
-                            I am thrilled to welcome you to our world of creativity, visual storytelling, 
-                            and the art of capturing moments that last a lifetime.
-                            <br/><br/>
-                            Photography has been my passion for as long as I can remember. 
-                            It's not just about taking pictures; it's about preserving emotions, experiences, and memories. 
-                            Every photograph we create at Eyecatcher is a piece of our heart and soul, 
-                            reflecting the unique beauty and essence of every moment.
-                        </span>
+                        </Typography>
+                        <Typography>
+                            We believe that remarkable photography should be accessible to all
+                            whether it's a special event, a family portrait, or a personal photoshoot, 
+                            our photography services are customized to meet your unique needs.
+                            Explore our budget-friendly pricing options to bring your visions to life.
+                        </Typography>
+                        <Button 
+                            className='intro-button' 
+                            endDecorator={<FontAwesomeIcon icon='chevron-right' />}
+                        >
+                            Learn more
+                        </Button>
                     </div>
                 </div>
+                <div className='grid-item item2'>
+                    <img src='/images/intro-image1.jpg' alt='Jasper and Natasha' className='intro-image1'/>
+                </div>
+            </div>
+            <Divider className='custom-MUIDivider'/>
+            <div className='container'>
+            
             </div>
         </div>
+        
+        // <div style={{marginTop: '80px'}}>
+        //     <div className="grid-container">
+        //         <div className="grid-item item1">
+        //             <div className='intro-quote1'>
+                        // <Typography className='intro-quote'>
+                        // Photography is the <br/> <strong className='intro-quote colored-font'> art of froz</strong>
+                        // <span 
+                        //     className='intro-quote' 
+                        //     style={{
+                        //         textDecoration: 'underline',
+                        //         textUnderlineOffset: '10px',
+                        //         textDecorationThickness: '4px',
+                        //         color: 'var(--blue)'
+                        //     }}
+                        // >
+                        //     <strong className='intro-quote colored-font'>en time</strong>
+                        // </span>
+                        // </Typography>
+        //             </div>
+        //         </div>
+        //         <div className="grid-item item2">
+        //             <img src='/images/intro-image1.jpg' alt='Jasper and Natasha' className='intro-image1'/>
+        //         </div>
+        //         <div className="grid-item item3">
+        //             <img src='/images/intro-image2.jpg' alt='Rodney and Zyrah' className='intro-image2'/>
+        //         </div>
+        //         <div className="grid-item item4">
+        //             <div className='intro-quote2'>
+        //                 <Typography className='intro-quote'>
+        //                 Where the science of light  <br/>
+        //                 <span 
+        //                     className='intro-quote' 
+        //                     style={{
+        //                         borderBottom: '4px solid var(--blue)',
+        //                         textUnderlineOffset: '10px',
+        //                         textDecorationThickness: '4px'
+        //                     }}
+        //                 >
+        //                     meets t
+        //                 </span>
+        //                 he <strong className='intro-quote colored-font'>art of emotion</strong>
+        //                 </Typography>
+        //             </div>
+        //             <Button className='intro-button' endDecorator={<FontAwesomeIcon icon='chevron-right'></FontAwesomeIcon>}>Learn more</Button>
+        //         </div>
+        //     </div>
+        //     <div className='container' style={{display: 'flex', justifyContent: 'center'}}>
+        //         <BioCard />
+        //         {/* <div className='founder-message-box'>
+        //             <img className='founder-image founder-message-box-element' src='/images/founder-image.jpg' />
+        //             <div className='founder-message-div founder-message-box-element'>
+        //                 <span className='founder-message-title'>
+        //                     <Typography className='intro-quote'>
+        //                     I am Jes Sta Isabel-Pacios  <br/>
+        //                     <span 
+        //                         className='intro-quote' 
+        //                         style={{
+        //                             textDecoration: 'underline',
+        //                             textUnderlineOffset: '10px',
+        //                             textDecorationThickness: '4px'
+        //                         }}
+        //                     >
+        //                         found
+        //                     </span>
+        //                     er of Eyecatcher Photography
+        //                     </Typography>
+        //                 </span>
+        //                 <span className='founder-message'>
+        //                     I am Jes Sta Isabel-Pacios, the founder of Eyecatcher Photography. 
+        //                     I am thrilled to welcome you to our world of creativity, visual storytelling, 
+        //                     and the art of capturing moments that last a lifetime.
+        //                     <br/><br/>
+        //                     Photography has been my passion for as long as I can remember. 
+        //                     It's not just about taking pictures; it's about preserving emotions, experiences, and memories. 
+        //                     Every photograph we create at Eyecatcher is a piece of our heart and soul, 
+        //                     reflecting the unique beauty and essence of every moment.
+        //                 </span>
+        //             </div>
+        //         </div> */}
+        //     </div>
+        // </div>
     );
 }
--- eyecatcher-photography-ui/src/app/layout/NavBar.tsx
+++ eyecatcher-photography-ui/src/app/layout/NavBar.tsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
 import { Dropdown, Menu, MenuButton } from '@mui/base';
 import { StyledListbox, StyledMenuItem } from '../common/MUIStyledComponent';
-import { Box, Drawer, ListItem, Typography } from '@mui/joy';
+import { Box, Drawer, IconButton, ListItem, SvgIcon, Typography } from '@mui/joy';
 import { getCurrentUser, logout } from '../services/authService';
 import { Customer } from '../models/Customer';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
@@ -17,6 +17,7 @@ interface DrawerItem{
 
 export default function NavBar(){
   const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
+  const [scrolled, setScrolled] = useState(false);
   const [open, setOpen] = useState(false);
   const [customer, setCustomer] = useState<Customer | null>();
   const [showUserDropdown, setShowUserDropdown] = useState(false);
@@ -79,6 +80,23 @@ export default function NavBar(){
   };
 
   useEffect(() => {
+    // Remove box shadow of navbar when at starting pos
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
     // Logic for hiding nav when scroll down, showing nav when scroll up
     let navBar = document.getElementById("navbar") as HTMLElement;
     if (navBar){
@@ -110,18 +128,20 @@ export default function NavBar(){
   },[])
 
   return(
-      <nav className="navbar" id='navbar'>
+      <nav className={`navbar ${scrolled ? 'with-box-shadow' : ''}`} id='navbar'>
       <div className="logo" onClick={()=>{window.location.href = '/'}}>
           <Typography
-            fontWeight="md"
-            textColor='white'
+            fontWeight="bolder"
+            textColor='black'
+            fontFamily='Dancing Script'
+            fontSize='20px'
             startDecorator={
               <Box
                 component="span"
                 sx={{
                   width: '50px',
                   height: '50px',
-                  backgroundImage: `url("/images/logowhite.png")`,
+                  backgroundImage: `url("/images/logoblack.png")`,
                   backgroundSize: "110%",
                   backgroundRepeat: "no-repeat"
                 }}
@@ -139,17 +159,11 @@ export default function NavBar(){
           <li>
             <Link to="/">Cart</Link>
           </li>
-          <li>
-            <Link to="/">Support</Link>
-          </li>
-          <li>
-            <Link to="/">About</Link>
-          </li>
         </div>
       </ul>
 
       <div className="right-nav">
-        <div className="user-picture-div">
+        {/* <div className="user-picture-div">
           <Dropdown>
             <MenuButton>
               <FontAwesomeIcon className="user-picture" icon="user-circle" onClick={toggleUserDropdown}></FontAwesomeIcon>
@@ -170,21 +184,68 @@ export default function NavBar(){
             </Menu>
           </Dropdown>
 
-        </div>
-        <input type="checkbox" id="checkbox_toggle" onClick={toggleDrawer(true)}/>
-        <label
-          htmlFor="checkbox_toggle"
-          className="hamburger"
-          style={{ fontSize: 32 }}
-        >
-          ☰
-        </label>
-        {isSmallScreen && <Drawer open={open} onClose={toggleDrawer(false)}>
+        </div> */}
+
+        {!isSmallScreen && 
+        <>
+          <IconButton size="sm" variant="plain" color="neutral" onClick={() => {window.open('https://www.facebook.com/eyecatchyou2020', '_blank');}}>
+            <SvgIcon sx={{fontSize: '30px', color: 'black'}}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M14 13.5h2.5l1-4H14v-2c0-1.03 0-2 2-2h1.5V2.14c-.326-.043-1.557-.14-2.857-.14C11.928 2 10 3.657 10 6.7v2.8H7v4h3V22h4v-8.5Z"
+                />
+              </svg>
+            </SvgIcon>
+          </IconButton> 
+          
+          <IconButton size="sm" variant="plain" color="neutral" onClick={() => {window.open('https://www.instagram.com/eyecatcher.photography/', '_blank');}}>
+            <SvgIcon sx={{fontSize: '30px', color: 'black'}}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M12 6.865A5.135 5.135 0 1 0 17.135 12A5.135 5.135 0 0 0 12 6.865Zm0 8.469A3.334 3.334 0 1 1 15.334 12A3.333 3.333 0 0 1 12 15.334Z"
+                />
+                <path
+                  fill="currentColor"
+                  d="M21.94 7.877a7.333 7.333 0 0 0-.465-2.427a4.918 4.918 0 0 0-1.153-1.772a4.894 4.894 0 0 0-1.77-1.153a7.323 7.323 0 0 0-2.428-.464C15.058 2.012 14.717 2 12.001 2s-3.057.011-4.123.06a7.333 7.333 0 0 0-2.428.465a4.905 4.905 0 0 0-1.77 1.153A4.886 4.886 0 0 0 2.525 5.45a7.333 7.333 0 0 0-.464 2.427c-.05 1.066-.06 1.407-.06 4.123s.01 3.057.06 4.123a7.334 7.334 0 0 0 .464 2.427a4.888 4.888 0 0 0 1.154 1.772a4.917 4.917 0 0 0 1.771 1.153a7.338 7.338 0 0 0 2.428.464C8.944 21.988 9.285 22 12 22s3.057-.011 4.123-.06a7.333 7.333 0 0 0 2.427-.465a5.113 5.113 0 0 0 2.925-2.925a7.316 7.316 0 0 0 .465-2.427c.048-1.067.06-1.407.06-4.123s-.012-3.057-.06-4.123Zm-1.8 8.164a5.549 5.549 0 0 1-.343 1.857a3.311 3.311 0 0 1-1.898 1.898a5.522 5.522 0 0 1-1.857.344c-1.055.048-1.371.058-4.042.058s-2.986-.01-4.04-.058a5.526 5.526 0 0 1-1.857-.344a3.108 3.108 0 0 1-1.15-.748a3.085 3.085 0 0 1-.748-1.15a5.521 5.521 0 0 1-.344-1.857c-.048-1.054-.058-1.37-.058-4.04s.01-2.987.058-4.042a5.563 5.563 0 0 1 .344-1.857a3.107 3.107 0 0 1 .748-1.15a3.082 3.082 0 0 1 1.15-.748A5.523 5.523 0 0 1 7.96 3.86C9.014 3.81 9.331 3.8 12 3.8s2.987.011 4.042.059a5.564 5.564 0 0 1 1.857.344a3.31 3.31 0 0 1 1.898 1.898a5.523 5.523 0 0 1 .344 1.857c.048 1.055.058 1.37.058 4.041s-.01 2.986-.058 4.041ZM17.339 5.462Z"
+                />
+              </svg>
+            </SvgIcon>
+          </IconButton>
+
+          <button className='sharp-button'>
+            Contact
+          </button>
+        </>}
+
+        <FontAwesomeIcon 
+          icon='bars' 
+          style={{
+            fontSize: '30px', 
+            color: 'black',
+            cursor: 'pointer'
+          }}
+          onClick={toggleDrawer(true)}
+        />
+        <Drawer open={open} onClose={toggleDrawer(false)}>
           <Box
             role="presentation"
             onClick={toggleDrawer(false)}
             onKeyDown={toggleDrawer(false)}
-            sx={{margin: '50px 20px 20px 20px'}}
+            sx={{
+              margin: '50px 20px 20px 20px'
+           }}
           > 
             <div className='user-info-drawer'>
               <div>
@@ -202,6 +263,11 @@ export default function NavBar(){
                       key={drawerItem.text}
                       className="custom-list-item"
                       onClick={drawerItem.location}
+                      sx={{
+                        '&:hover': {
+                          cursor: 'pointer'
+                        }
+                      }}
                     >
                       <FontAwesomeIcon icon={drawerItem.icon} style={{fontSize: '20px', margin: 'auto'}} />
                       <span style={{fontWeight: '600'}}>{drawerItem.text}</span>
@@ -213,13 +279,18 @@ export default function NavBar(){
               <ListItem
                 className="custom-list-item"
                 onClick={logout}
+                sx={{
+                  '&:hover': {
+                    cursor: 'pointer'
+                  }
+                }}
               >
-                  <FontAwesomeIcon icon='sign-out' style={{fontSize: '20px', margin: 'auto'}} />
+                  <FontAwesomeIcon icon='sign-out' style={{fontSize: '20px', margin: 'auto', width: '90%'}} />
                   <span style={{fontWeight: '600'}}>Logout</span>
               </ListItem>
             </ul>
           </Box>
-        </Drawer>}
+        </Drawer>
       </div>
     </nav>
   );
